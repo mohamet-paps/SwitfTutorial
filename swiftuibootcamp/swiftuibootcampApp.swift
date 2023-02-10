36bc320a171f66208581c008193ab2c4b43ac660
mohamet <mohamed.diatta@paps-app.com>
2023-02-10 21:57:44 +0000

    Initial Commit

---
+++ swiftuibootcamp/swiftuibootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuibootcampApp.swift
+//  swiftuibootcamp
+//
+//  Created by Mohamet Diatta on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftuibootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
